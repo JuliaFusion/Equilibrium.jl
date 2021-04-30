9b0f05ac67fa9b8b38f189f76af6d2b7f127b8dc
Luke Stagner <stagnerl@fusion.gat.com>
2021-04-29 21:26:43 -0700

    curlB = m0J tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,14 +13,37 @@ M2 = AxisymmetricEquilibrium(g2)
 M3 = AxisymmetricEquilibrium(g3)
 M4 = AxisymmetricEquilibrium(g4)
 
-@testset "Bt-Ip Signs" begin
-    @test (sign(g1.bcentr), sign(g1.current)) == (1, 1)
-    @test (sign(g2.bcentr), sign(g2.current)) == (-1, 1)
-    @test (sign(g3.bcentr), sign(g3.current)) == (1, -1)
-    @test (sign(g4.bcentr), sign(g4.current)) == (-1, -1)
+r1 = (M1.axis[1]+0.1,M1.axis[2])
+r2 = (M2.axis[1]+0.1,M2.axis[2])
+r3 = (M3.axis[1]+0.1,M3.axis[2])
+r4 = (M4.axis[1]+0.1,M4.axis[2])
 
-    @test M1.sigma == 1
-    @test M2.sigma == -1
-    @test M3.sigma == -1
-    @test M4.sigma == 1
+@testset verbose = true "Equilibrium Tests" begin
+    @testset "Bt-Ip Signs" begin
+        @test (sign(g1.bcentr), sign(g1.current)) == (1, 1)
+        @test (sign(g2.bcentr), sign(g2.current)) == (-1, 1)
+        @test (sign(g3.bcentr), sign(g3.current)) == (1, -1)
+        @test (sign(g4.bcentr), sign(g4.current)) == (-1, -1)
+
+        Bt1, Ip1 = (sign(Bfield(M1,r1...)[2]), sign(Jfield(M1,r1...)[2]))
+        @test (Bt1, Ip1) == (1, 1)
+
+        (Bt2, Ip2) = (sign(Bfield(M2,r2...)[2]), sign(Jfield(M2,r2...)[2]))
+        @test (Bt2, Ip2) == (-1, 1)
+
+        (Bt3, Ip3) = (sign(Bfield(M3,r3...)[2]), sign(Jfield(M3,r2...)[2]))
+        @test (Bt3, Ip3) == (1, -1)
+
+        (Bt4, Ip4) = (sign(Bfield(M4,r4...)[2]), sign(Jfield(M4,r4...)[2]))
+        @test (Bt4, Ip4) == (-1, -1)
+    end
+
+    @testset verbose = true "CurlB == μ₀J" begin
+        mu0 = 4pi*10^-7
+        @test curlB(M1,r1...) ≈ mu0*Jfield(M1,r1...) rtol=0.01
+        @test curlB(M2,r2...) ≈ mu0*Jfield(M2,r2...) rtol=0.01
+        @test curlB(M3,r3...) ≈ mu0*Jfield(M3,r3...) rtol=0.01
+        @test curlB(M4,r4...) ≈ mu0*Jfield(M4,r4...) rtol=0.01
+    end
 end
+
